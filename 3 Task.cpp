947845522578bddab5ceca20a5a8063ebfb2ece2
Onhisneck <romanpayolov@gmail.com>
2026-06-18 22:54:57 +0800

    Сделал через массив

---
+++ 3 Task.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int n, k;
+    std::cin >> n >> k;
+
+    std::vector<long long> snow(n + 1, 0);
+
+    for (int q = 0; q < k; q++) {
+        int type;
+        std::cin >> type;
+
+        if (type == 1) {
+            int i, x;
+            std::cin >> i >> x;
+            snow[i] += x;
+        } else {
+            int u, r;
+            std::cin >> u >> r;
+
+            long long sum = 0;
+            for (int i = u; i <= r; i++) {
+                sum += snow[i];
+            }
+            std::cout << sum << std::endl;
+        }
+    }
+
+    return 0;
+}
